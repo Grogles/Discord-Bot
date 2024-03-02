24ee3a0c7fcb0a8bc4f7a5371f019ffd99dc760b
Grogles <jefferyq229@gmail.com>
2024-03-02 00:08:30 -0500

    Made add command functional

--- src/index.js
+++ src/index.js
@@ -34,4 +34,10 @@ client.on('interactionCreate', (interaction) => {
     if (interaction.commandName === 'ping') {
         interaction.reply("Pong!");
     }
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+
+        interaction.reply(`The sum is ${num1 + num2}`);
+    }
 });
